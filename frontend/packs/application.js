70b23356fa4b5b4e7b1ae2e80645c8e05bab354d
Peter McCready <petermccready2@gmail.com>
2018-08-23 21:57:17 +0100

    Render home view within page component

--- frontend/packs/application.js
+++ frontend/packs/application.js
@@ -1,5 +1,5 @@
 import "init";
-import "../components/page/page";
+import "components/page/page";
 
 /* eslint no-console:0 */
 // This file is automatically compiled by Webpack, along with any other files
