adea14836e6284db447b7b7168671bbae8780ba2
Peter McCready <petermccready2@gmail.com>
2018-08-23 19:15:46 +0100

    Setup webpack

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  # Tell rails app where to find View partials
+  prepend_view_path Rails.root.join('frontend')
 end
--- frontend/packs/application.js
+++ frontend/packs/application.js
@@ -7,4 +7,10 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
+// webpack entry point
+
 console.log('Hello World from Webpacker')
+
+import './application.css';
+
+document.body.insertAdjacentHTML('afterbegin', 'Some stuff!');
