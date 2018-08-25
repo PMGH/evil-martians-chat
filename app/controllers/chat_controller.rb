ef4bef2a8274f696150760ac718f53e3358b385f
Peter McCready <petermccready2@gmail.com>
2018-08-25 16:30:46 +0100

    Render @messages as JSON with 200

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -1,2 +1,16 @@
 class ChatController < ApplicationController
+  before_action :authenticate!
+
+  # Display last 20 messages
+  def show
+    @messages = Message.order(created_at: :asc).last(20)
+    render status: 200, json: @messages
+  end
+
+  private
+
+  # Redirect user to /login if they hadn't picked a username yet
+  def authenticate!
+    redirect_to login_path unless session[:username]
+  end
 end
