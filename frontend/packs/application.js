af4172a3583d99d4f471e967c645977cc7a53454
Peter McCready <petermccready2@gmail.com>
2018-08-25 16:22:50 +0100

    Add login page with styling

--- frontend/packs/application.js
+++ frontend/packs/application.js
@@ -1,5 +1,6 @@
 import "init";
 import "components/page/page";
+import "components/auth-form/auth-form";
 
 /* eslint no-console:0 */
 // This file is automatically compiled by Webpack, along with any other files
