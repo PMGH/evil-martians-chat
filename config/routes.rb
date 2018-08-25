63ce7959d05258045093823af49584ea921a86b5
Peter McCready <petermccready2@gmail.com>
2018-08-25 15:33:36 +0100

    Amend routes to include chat and login routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root to: 'pages#home'
-  get 'pages/home'
+  root to: 'chat#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/login', to: 'auth#new'
+  post '/login', to: 'auth#create'
 end
